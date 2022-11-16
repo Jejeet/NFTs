c7528b09ea8b055334ab4a59acf2885367b6df4f
Adewumi <owonikokoadewunmi@gmail.com>
2022-11-16 12:18:17 +0100

    fixed footer

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -140,10 +140,10 @@ const home = () => {
 
 
       {/* the footer div */}
-      <div className=" bg-[#1D1D1E] h-[18rem]" >
+      <div className=" bg-[#1D1D1E] h-[18rem] " >
 
         {/* the links and logo */}
-        <div className="flex flex-row justify-between text-white px-12">
+        <div className="flex flex-row justify-between text-white px-12 md:flex-cols">
           <div>
             <div className="py-8">
               {" "}
